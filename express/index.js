5a344ac538d62b5ed3282ccdd498c979f1909e77
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-03 06:35:16 +0530

    add user model and implement MongoDB connection; update form handling to create users

--- express/index.js
+++ express/index.js
@@ -1,5 +1,8 @@
+import { log } from 'console';
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
+import { User } from './models/User.js';
 
 const port = 3002;
 const app = express();
@@ -7,6 +10,17 @@ const app = express();
 app.use(express.static(path.join(path.resolve(), 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose.connect(
+    "mongodb+srv://mnraza1907:joucZTUxihODcLTe@cluster0.i5oia.mongodb.net/", {
+    dbName: 'nodejs_mastery',
+}
+).then(() => {
+    console.log('Connected to MongoDB ✅');
+
+}).catch((err) => {
+    console.log('Error:', err);
+});
 
 const products = [
     {
@@ -30,12 +44,35 @@ app.get('/', (req, res) => {
 }
 );
 app.post('/form-submit', (req, res) => {
+    console.log(req.body);
     res.json(
         {
             message: 'Form submitted successfully',
             success: true
         });
 });
+
+app.post('/data', async (req, res) => {
+    console.log(req.body);
+    try {
+        let user = await User.create(req.body);
+        res.json({
+            message: 'User Created successfully ✅',
+            user: user,
+            success: true
+        });
+
+    } catch (error) {
+        res.json({
+            message: error.message,
+            success: false
+        });
+    }
+
+
+});
+
+
 app.get('/products', (req, res) => {
     res.json({ products });
 });
